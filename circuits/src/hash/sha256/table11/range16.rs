91cab4c519de0e151d4662f720c93749ef2abaf6
iquerejeta <querejeta.azurmendi@iohk.io>
2025-06-26 13:16:10 +0200

    Add copyright header to all files

--- circuits/src/hash/sha256/table11/range16.rs
+++ circuits/src/hash/sha256/table11/range16.rs
@@ -1,3 +1,16 @@
+// This file is part of MIDNIGHT-ZK.
+// Copyright (C) 2025 Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! This configure is used to constrain an assigned value as 16-bit long.
 //! An assigned value is firstly copy constrained with the cell in a_1
 //! then we decompose the value in chunks of 2-bit in the cells a_2, a_3,
