448a4567ded137e01c426ee94889a734241e584b
ambrona <miguel.ambrona@iohk.io>
2025-07-29 18:34:05 +0200

    circuits/sha256: introduce [sha256]

--- circuits/src/hash/new_sha256/sha256_chip.rs
+++ circuits/src/hash/new_sha256/sha256_chip.rs
@@ -23,12 +23,27 @@ use crate::{
         },
     },
     instructions::assignments::AssignmentInstructions,
-    types::AssignedNative,
+    types::{AssignedByte, AssignedNative},
     utils::{
         util::{fe_to_u32, fe_to_u64},
         ComposableChip,
     },
 };
+
+const ROUND_CONSTANTS: [u32; 64] = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+];
+
+const IV: [u32; 8] = [
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+];
 
 const NB_SHA256_ADVICE_COLS: usize = 7;
 const NB_SHA256_FIXED_COLS: usize = 2;
@@ -1006,7 +1021,7 @@ impl<F: PrimeField> Sha256Chip<F> {
         )?;
 
         let next_a_summands = [
-            state.h.plain.clone(),
+            state.h.clone(),
             Sigma_1_of_e.clone(),
             Ch_of_e_f_g.clone(),
             round_k.clone(),
@@ -1016,8 +1031,8 @@ impl<F: PrimeField> Sha256Chip<F> {
         ];
 
         let next_e_summands = [
-            state.d.plain.clone(),
-            state.h.plain.clone(),
+            state.d.clone(),
+            state.h.clone(),
             Sigma_1_of_e,
             Ch_of_e_f_g,
             round_k.clone(),
@@ -1028,11 +1043,79 @@ impl<F: PrimeField> Sha256Chip<F> {
             a: self.prepare_A(layouter, &next_a_summands)?,
             b: state.a.combined.clone(),
             c: state.b.clone(),
-            d: state.c.clone(),
+            d: state.c.plain.clone(),
             e: self.prepare_E(layouter, &next_e_summands)?,
             f: state.e.combined.clone(),
             g: state.f.clone(),
-            h: state.g.clone(),
+            h: state.g.plain.clone(),
+        })
+    }
+
+    /// Message schedule.
+    /// TODO
+    fn message_schedule(
+        &self,
+        _layouter: &mut impl Layouter<F>,
+        _input_bytes: &[AssignedByte<F>],
+    ) -> Result<Vec<[AssignedPlain<F, 32>; 64]>, Error> {
+        todo!()
+    }
+
+    /// SHA256 computation.
+    fn sha256(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        input_bytes: &[AssignedByte<F>],
+    ) -> Result<[AssignedPlain<F, 32>; 8], Error> {
+        let message_blocks = self.message_schedule(layouter, input_bytes)?;
+        let mut state = CompressionState::<F>::fixed(layouter, &self.native_chip, &IV)?;
+
+        for block in message_blocks {
+            let mut compression_state = state.clone();
+            for i in 0..64 {
+                compression_state = self.compression_round(
+                    layouter,
+                    &compression_state,
+                    ROUND_CONSTANTS[i],
+                    &block[i],
+                )?;
+            }
+            state = state.add(&self, layouter, &compression_state)?;
+        }
+
+        Ok(state.plain())
+    }
+}
+
+impl<F: PrimeField> CompressionState<F> {
+    /// Adds pair-wise (modulo 2^32) the fields of two compression states.
+    fn add(
+        &self,
+        sha256_chip: &Sha256Chip<F>,
+        layouter: &mut impl Layouter<F>,
+        other: &Self,
+    ) -> Result<Self, Error> {
+        let a = sha256_chip.prepare_A(layouter, &[self.a.plain(), other.a.plain()])?;
+        let b = sha256_chip.prepare_A(layouter, &[self.b.plain.clone(), other.b.plain.clone()])?;
+        let c = sha256_chip.prepare_A(layouter, &[self.c.plain.clone(), other.c.plain.clone()])?;
+        let d = sha256_chip.prepare_A(layouter, &[self.d.clone(), other.d.clone()])?;
+        // TODO: d can be optimized and do it in a single row without `prepare_A`.
+
+        let e = sha256_chip.prepare_E(layouter, &[self.e.plain(), other.e.plain()])?;
+        let f = sha256_chip.prepare_E(layouter, &[self.f.plain.clone(), other.f.plain.clone()])?;
+        let g = sha256_chip.prepare_E(layouter, &[self.g.plain.clone(), other.g.plain.clone()])?;
+        let h = sha256_chip.prepare_E(layouter, &[self.h.clone(), other.h.clone()])?;
+        // TODO: h can be optimized and do it in a single row without `prepare_E`.
+
+        Ok(Self {
+            a,
+            b: b.combined,
+            c: c.combined,
+            d: d.combined.plain,
+            e,
+            f: f.combined,
+            g: g.combined,
+            h: h.combined.plain,
         })
     }
 }
--- circuits/src/hash/new_sha256/types.rs
+++ circuits/src/hash/new_sha256/types.rs
@@ -1,6 +1,11 @@
 use ff::PrimeField;
+use midnight_proofs::{circuit::Layouter, plonk::Error};
 
-use crate::field::AssignedNative;
+use crate::{
+    field::{AssignedNative, NativeChip},
+    hash::new_sha256::utils::{spread, u32_in_be_limbs},
+    instructions::AssignmentInstructions,
+};
 
 /// Assigned plain value of given number of bits L.
 #[derive(Clone, Debug)]
@@ -41,13 +46,127 @@ pub(super) struct LimbsOfE<F: PrimeField> {
     pub spreaded_limb_06: AssignedSpreaded<F, 6>,
 }
 
+#[derive(Clone, Debug)]
 pub(super) struct CompressionState<F: PrimeField> {
     pub(super) a: LimbsOfA<F>,
     pub(super) b: AssignedPlainSpreaded<F, 32>,
     pub(super) c: AssignedPlainSpreaded<F, 32>,
-    pub(super) d: AssignedPlainSpreaded<F, 32>,
+    pub(super) d: AssignedPlain<F, 32>,
     pub(super) e: LimbsOfE<F>,
     pub(super) f: AssignedPlainSpreaded<F, 32>,
     pub(super) g: AssignedPlainSpreaded<F, 32>,
-    pub(super) h: AssignedPlainSpreaded<F, 32>,
+    pub(super) h: AssignedPlain<F, 32>,
+}
+
+impl<F: PrimeField, const N: usize> AssignedPlain<F, N> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        c: u32,
+    ) -> Result<Self, Error> {
+        assert!(c < (1 << N));
+        Ok(Self(native_chip.assign_fixed(layouter, F::from(c as u64))?))
+    }
+}
+
+impl<F: PrimeField, const N: usize> AssignedSpreaded<F, N> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        c: u32,
+    ) -> Result<Self, Error> {
+        assert!(c < (1 << N));
+        Ok(Self(
+            native_chip.assign_fixed(layouter, F::from(spread(c)))?,
+        ))
+    }
+}
+
+impl<F: PrimeField, const N: usize> AssignedPlainSpreaded<F, N> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        c: u32,
+    ) -> Result<Self, Error> {
+        assert!(c < (1 << N));
+        Ok(Self {
+            plain: AssignedPlain::<F, N>::fixed(layouter, native_chip, c)?,
+            spreaded: AssignedSpreaded::<F, N>::fixed(layouter, native_chip, c)?,
+        })
+    }
+}
+
+impl<F: PrimeField> LimbsOfA<F> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        constant: u32,
+    ) -> Result<Self, Error> {
+        let [c10, c09, c11, c02] = u32_in_be_limbs(constant, [10, 9, 11, 2]);
+        Ok(Self {
+            combined: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, constant)?,
+            spreaded_limb_10: AssignedSpreaded::<F, 10>::fixed(layouter, native_chip, c10)?,
+            spreaded_limb_09: AssignedSpreaded::<F, 9>::fixed(layouter, native_chip, c09)?,
+            spreaded_limb_11: AssignedSpreaded::<F, 11>::fixed(layouter, native_chip, c11)?,
+            spreaded_limb_02: AssignedSpreaded::<F, 2>::fixed(layouter, native_chip, c02)?,
+        })
+    }
+
+    pub(super) fn plain(&self) -> AssignedPlain<F, 32> {
+        self.combined.plain.clone()
+    }
+}
+
+impl<F: PrimeField> LimbsOfE<F> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        constant: u32,
+    ) -> Result<Self, Error> {
+        let [c07, c12, c02, c05, c06] = u32_in_be_limbs(constant, [7, 12, 2, 5, 6]);
+        Ok(Self {
+            combined: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, constant)?,
+            spreaded_limb_07: AssignedSpreaded::<F, 7>::fixed(layouter, native_chip, c07)?,
+            spreaded_limb_12: AssignedSpreaded::<F, 12>::fixed(layouter, native_chip, c12)?,
+            spreaded_limb_02: AssignedSpreaded::<F, 2>::fixed(layouter, native_chip, c02)?,
+            spreaded_limb_05: AssignedSpreaded::<F, 5>::fixed(layouter, native_chip, c05)?,
+            spreaded_limb_06: AssignedSpreaded::<F, 6>::fixed(layouter, native_chip, c06)?,
+        })
+    }
+
+    pub(super) fn plain(&self) -> AssignedPlain<F, 32> {
+        self.combined.plain.clone()
+    }
+}
+
+impl<F: PrimeField> CompressionState<F> {
+    pub(super) fn fixed(
+        layouter: &mut impl Layouter<F>,
+        native_chip: &NativeChip<F>,
+        v: &[u32; 8],
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            a: LimbsOfA::<F>::fixed(layouter, native_chip, v[0])?,
+            b: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, v[1])?,
+            c: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, v[2])?,
+            d: AssignedPlain::<F, 32>::fixed(layouter, native_chip, v[3])?,
+            e: LimbsOfE::<F>::fixed(layouter, native_chip, v[4])?,
+            f: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, v[5])?,
+            g: AssignedPlainSpreaded::<F, 32>::fixed(layouter, native_chip, v[6])?,
+            h: AssignedPlain::<F, 32>::fixed(layouter, native_chip, v[7])?,
+        })
+    }
+
+    pub(super) fn plain(self) -> [AssignedPlain<F, 32>; 8] {
+        [
+            self.a.combined.plain,
+            self.b.plain,
+            self.c.plain,
+            self.d,
+            self.e.combined.plain,
+            self.f.plain,
+            self.g.plain,
+            self.h,
+        ]
+    }
 }
