eb6f677df8c9a857cc2d00546f88443b04535a90
David Nevado <david@davidnevado.xyz>
2025-08-21 18:35:14 +0200

    add: AtalaEnrollment example
    fix: rebase

--- circuits/examples/identity/enrollment.rs
+++ circuits/examples/identity/enrollment.rs
@@ -1,32 +1,30 @@
-//! Example of enrollment proof on an Identus credential.
-//! The enrollment proof verifies:
-//!  - The ECDSA signature of the credential.
-//!  - The Base64 decoding of the blob into JSON format.
-//!  - The validity of the JSON structure.
+//! Example of verifying the validity of an ECDSA signed Atala identity JSON.
 
+use std::time::Instant;
 use std::{fs::OpenOptions, io::Read};
 
 use halo2curves::secp256k1::{Fq as secp256k1Scalar, Secp256k1};
+use midnight_circuits::instructions::public_input::CommittedInstanceInstructions;
 use midnight_circuits::{
     compact_std_lib::{self, Relation, ShaTableSize, ZkStdLib, ZkStdLibArch},
     field::foreign::{params::MultiEmulationParams, AssignedField},
     instructions::{
         ArithInstructions, AssertionInstructions, AssignmentInstructions, Base64Instructions,
         DecompositionInstructions, EccInstructions, PublicInputInstructions,
-        RangeCheckInstructions,
     },
-    parsing::{DateFormat, Separator},
     testing_utils::{
         ecdsa::{ECDSASig, Ecdsa, FromBase64, PublicKey},
         plonk_api::filecoin_srs,
     },
-    types::{AssignedByte, AssignedForeignPoint, InnerValue, Instantiable},
+    types::{AssignedByte, AssignedForeignPoint, Instantiable},
 };
+use midnight_curves::G1Affine;
+use midnight_proofs::plonk::commit_to_instances;
+use midnight_proofs::poly::kzg::KZGCommitmentScheme;
 use midnight_proofs::{
     circuit::{Layouter, Value},
     plonk::Error,
 };
-use num_bigint::BigUint;
 use rand::rngs::OsRng;
 use sha2::Digest;
 
@@ -38,14 +36,6 @@ const CRED_PATH: &str = "./examples/identity/credentials/2k-credential";
 const PUB_KEY: &[u8] =
     b"_bDXlQJ636HHOvXSe-flG0f-OkkRu8Jusm93PB2GBjoykg753nsOiW1vhEpCnxxybkMdarJLXIUJIYw1K2emQI";
 
-// Secret key of the credential holder.
-const HOLDER_SK: SK = SK::from_raw([
-    0x87c251f40ac6a55e,
-    0xc82dbae785c00836,
-    0x36f09fcb94100833,
-    0xc4e05a8ec16835ce,
-]);
-
 const HEADER_LEN: usize = 38;
 const PAYLOAD_LEN: usize = 2463;
 
@@ -53,18 +43,26 @@ const PAYLOAD_LEN: usize = 2463;
 type PK = Secp256k1;
 // Credential payload.
 type Payload = [u8; PAYLOAD_LEN];
-// Holder secret key.
-type SK = secp256k1Scalar;
 
+/// This relation checks the validity of an Identus credential.
+/// It receives as public inputs the public key of the credential signer and
+/// the decoded JSON of the credential in committed form.
 #[derive(Clone, Default)]
-pub struct AtalaJsonECDSA;
+pub struct AtalaEnrollment;
 
-impl Relation for AtalaJsonECDSA {
+impl Relation for AtalaEnrollment {
     type Instance = PK;
-    type Witness = (Payload, ECDSASig, SK);
+    type Witness = (Payload, ECDSASig);
 
     fn format_instance(instance: &Self::Instance) -> Vec<F> {
         AssignedForeignPoint::<F, Secp256k1, MultiEmulationParams>::as_public_input(instance)
+    }
+
+    fn format_committed_instances(witness: &Self::Witness) -> Vec<F> {
+        let json_b64 = &witness.0[HEADER_LEN + 1..PAYLOAD_LEN];
+        let json = base64::decode_config(json_b64, base64::STANDARD_NO_PAD)
+            .expect("Valid base64 encoded JSON.");
+        json.iter().map(|byte| F::from(*byte as u64)).collect()
     }
 
     fn circuit(
@@ -80,11 +78,10 @@ impl Relation for AtalaJsonECDSA {
         // Assign the PK as public input.
         let pk = secp256k1_curve.assign_as_public_input(layouter, instance)?;
 
-        let payload = witness.map(|(payload, _, _)| payload).transpose_array();
-        let (sig, sk) = witness.map(|(_, sig, sk)| (sig, sk)).unzip();
+        let (payload, sig) = witness.unzip();
 
         // Assign payload.
-        let payload = std_lib.assign_many(layouter, &payload)?;
+        let payload = std_lib.assign_many(layouter, &payload.transpose_array())?;
 
         // Verify credential signature.
         Self::verify_ecdsa(std_lib, layouter, pk, &payload, sig)?;
@@ -93,7 +90,9 @@ impl Relation for AtalaJsonECDSA {
         let json =
             b64_chip.decode_base64(layouter, &payload[HEADER_LEN + 1..PAYLOAD_LEN], false)?;
 
-        // TODO Constrain as committed instance
+        for val in json.iter() {
+            std_lib.constrain_as_committed_public_input(layouter, val)?;
+        }
 
         Ok(())
     }
@@ -107,6 +106,7 @@ impl Relation for AtalaJsonECDSA {
             bls12_381: false,
             base64: true,
             nr_pow2range_cols: 3,
+            automaton: false,
         }
     }
 
@@ -115,11 +115,11 @@ impl Relation for AtalaJsonECDSA {
     }
 
     fn read_relation<R: std::io::Read>(_reader: &mut R) -> std::io::Result<Self> {
-        Ok(AtalaJsonECDSA)
+        Ok(AtalaEnrollment)
     }
 }
 
-impl AtalaJsonECDSA {
+impl AtalaEnrollment {
     /// Verifies the secp256k1 ECDSA signature of the given message.
     fn verify_ecdsa(
         std_lib: &ZkStdLib,
@@ -172,35 +172,54 @@ fn main() {
     let srs = filecoin_srs(K);
     let credential_blob = read_credential::<4096>(CRED_PATH).expect("Path to credential file.");
 
-    let relation = AtalaJsonECDSA;
+    let relation = AtalaEnrollment;
 
+    let start = |msg: &str| -> Instant {
+        println!("{msg}");
+        Instant::now()
+    };
+
+    let setup = start("Setting up the vk/pk");
     let vk = compact_std_lib::setup_vk(&srs, &relation);
     let pk = compact_std_lib::setup_pk(&relation, &vk);
+    println!("... done ({:?})", setup.elapsed());
 
     // Build the instance and witness to be proven.
+    let wit = start("Computing instance and witnesses");
     let instance = PublicKey::from_base64(PUB_KEY).expect("Base64 encoded PK");
-    let witness = AtalaJsonECDSA::witness_from_blob(credential_blob.as_slice());
-    let witness = (witness.0, witness.1, HOLDER_SK);
+    let witness = AtalaEnrollment::witness_from_blob(credential_blob.as_slice());
+    let witness = (witness.0, witness.1);
+    let committed_credential: G1Affine = {
+        let instance = AtalaEnrollment::format_committed_instances(&witness);
+        commit_to_instances::<_, KZGCommitmentScheme<_>>(&srs, vk.vk().get_domain(), &instance)
+            .into()
+    };
+    println!("... done ({:?})", wit.elapsed());
 
-    let proof = compact_std_lib::prove::<AtalaJsonECDSA, blake2b_simd::State>(
+    let p = start("Proof generation");
+    let proof = compact_std_lib::prove::<AtalaEnrollment, blake2b_simd::State>(
         &srs, &pk, &relation, &instance, witness, OsRng,
     )
     .expect("Proof generation should not fail");
+    println!("... done ({:?})", p.elapsed());
 
+    let v = start("Proof verification");
     assert!(
-        compact_std_lib::verify::<AtalaJsonECDSA, blake2b_simd::State>(
+        compact_std_lib::verify::<AtalaEnrollment, blake2b_simd::State>(
             &srs.verifier_params(),
             &vk,
             &instance,
+            Some(committed_credential),
             &proof
         )
         .is_ok()
-    )
+    );
+    println!("... done ({:?})", v.elapsed())
 }
 
 // Helper functions for base64 encoded credentials.
 // -----------------------------------------------
-impl AtalaJsonECDSA {
+impl AtalaEnrollment {
     // Creates an AtalaJsonECDSA witness from:
     // 1. A JWT encoded Atala credential.
     // 2. The corresponding base64 encoded ECDSA public key.
