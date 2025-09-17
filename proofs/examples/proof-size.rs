ac900d20bbc01ac4309a6d40e6f4d598dab7fcd0
iquerejeta <querejeta.azurmendi@iohk.io>
2025-09-17 10:23:07 +0200

    Change cost model
    Allow for a call to synthesise that does not require as input k. This avoid the need of looping over synthesize to determine the size of the circuit.

--- proofs/examples/proof-size.rs
+++ proofs/examples/proof-size.rs
@@ -80,12 +80,10 @@ impl Circuit<Fp> for TestCircuit {
     }
 }
 
-const K: u32 = 11;
-
 fn main() {
     let circuit = TestCircuit {};
 
-    let model = from_circuit_to_circuit_model::<_, _, 32, 32>(Some(K), &circuit, 10);
+    let model = from_circuit_to_circuit_model::<_, _, 32, 32>(&circuit);
     println!(
         "Cost of circuit with 8 bit lookup table: \n{}",
         serde_json::to_string_pretty(&model).unwrap()
