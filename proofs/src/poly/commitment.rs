933f2840203b0ed392c4b7566d7516f3a9791fd2
reinluft <reinluft.mark@gmail.com>
2025-11-11 14:42:13 +0100

    Prepare [Polynomial] and [PolynomialCommitmentScheme]

--- proofs/src/poly/commitment.rs
+++ proofs/src/poly/commitment.rs
@@ -34,7 +34,10 @@ pub trait PolynomialCommitmentScheme<F: PrimeField>: Clone + Debug {
     /// Verification guard. Allows for batch verification
     type VerificationGuard: Guard<F, Self>;
 
-    /// Generates the parameters of the polynomial commitment scheme
+    /// Return commitment to the constant polynomial `P(X) = 1`.
+    fn constant_commitment() -> Self::Commitment;
+
+    /// Generates the parameters of the polynomial commitment scheme.
     fn gen_params(k: u32) -> Self::Parameters;
 
     /// Extract the `VerifierParameters` from `Parameters`
