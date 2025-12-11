d112c68bdf1cdad1346b9591255d6cc1560ab506
Zhiyong-Gong <63758161+John-Gong-Math@users.noreply.github.com>
2025-12-11 17:25:52 -0500

    test passed for prepare_spreaded gate

--- circuits/src/hash/ripemd160/ripemd160_chip.rs
+++ circuits/src/hash/ripemd160/ripemd160_chip.rs
@@ -19,10 +19,10 @@ use crate::{
             spreaded_sum, u32_in_be_limbs,
         },
     },
-    instructions::DecompositionInstructions,
+    instructions::{AssignmentInstructions, DecompositionInstructions, EqualityInstructions},
     types::AssignedByte,
     utils::{
-        util::{fe_to_u64, u32_to_fe, u64_to_fe},
+        util::{fe_to_u32, fe_to_u64, u32_to_fe, u64_to_fe},
         ComposableChip,
     },
 };
@@ -243,6 +243,66 @@ impl<F: PrimeField> RipeMD160Chip<F> {
             .unwrap())
     }
 
+    /// Given an assigned word A, this function prepares its spreaded form.
+    fn prepare_spreaded(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        word: &AssignedWord<F>,
+    ) -> Result<AssignedSpreaded<F, 32>, Error> {
+        /*
+         Given assigned word A, we first compute its spreaded form ~A, and then
+         apply [`assign_sprdd_sum`] to the assigned values of ~A, ~0, ~0 as follows:
+
+        | T0 |    A0   |     A1   | T1 |   A2   |   A3   |  A4 | A5 | A6 | A7 |
+        |----|---------|----------|----|--------|--------|-----|----|----|----|
+        | 11 | Evn.11a | ~Evn.11a | 11 |   0    |   ~0   | Evn | ~A | ~0 | ~0 |
+        | 11 | Evn.11a | ~Evn.11a | 11 |   0    |   ~0   |     |    |    |    | <- q_spr_add, q_11_11_10
+        | 10 | Evn.11a | ~Evn.11a | 10 |   0    |   ~0   |     |    |    |    |
+
+        with constraints of:
+
+          1) applying the plain-spreaded lookup on 11-11-10 limbs of Evn and Odd:
+              Evn: (Evn.11a, Evn.11b, Evn.10)
+              Odd: (Odd.11a, Odd.11b, Odd.10)
+
+         2) asserting the 11-11-10 decomposition identity for Evn:
+               2^21 * Evn.11a + 2^10 * Evn.11b + Evn.10
+             = Evn
+
+         3) asserting the spr_sum_evn identity:
+               (4^21 * ~Evn.11a + 4^10 * ~Evn.11b + ~Evn.10) +
+           2 * (4^21 * ~0       + 4^10 * ~0       + ~0     )
+              = ~A + ~0 + ~0
+
+         4) asserting that:
+                 Evn = A
+         */
+        let sprdd_val = word.0.value().map(|&w| spread(fe_to_u32(w))).map(u64_to_fe);
+
+        let v: Vec<AssignedSpreaded<F, 32>> = self
+            .native_gadget
+            .assign_many(
+                layouter,
+                &[sprdd_val, Value::known(F::ZERO), Value::known(F::ZERO)],
+            )?
+            .into_iter()
+            .map(AssignedSpreaded)
+            .collect();
+        let [sprdd_word, sprdd_zero_a, sprdd_zero_b]: [AssignedSpreaded<F, 32>; 3] =
+            v.try_into().unwrap();
+
+        let even = self.assign_sprdd_sum(
+            layouter,
+            Parity::Evn,
+            &sprdd_word,
+            &sprdd_zero_a,
+            &sprdd_zero_b,
+        )?;
+
+        let _ = self.native_gadget.is_equal(layouter, &word.0, &even.0)?;
+        Ok(sprdd_word)
+    }
+
     /// Given three assigned spreaded ~A, ~B, ~C, this function computes the
     /// value of ~A + ~B + ~C, and fills a lookup table with the limbs of its
     /// even and odd parts (or vice versa) and returns the former or the
@@ -264,7 +324,7 @@ impl<F: PrimeField> RipeMD160Chip<F> {
         | 11 | Evn.11b | ~Evn.11b | 11 | Odd.11b | ~Odd.11b |     |     |    |    | <- q_11_11_10, q_spr_sum_evn
         | 10 | Evn.10  | ~Evn.10  | 10 | Odd.10  | ~Odd.10  |     |     |    |    |
 
-        with constraints enforcing:
+        with constraints of:
 
         1) applying the plain-spreaded lookup on 11-11-10 limbs of Evn and Odd:
              Evn: (Evn.11a, Evn.11b, Evn.10)
@@ -289,7 +349,7 @@ impl<F: PrimeField> RipeMD160Chip<F> {
         | 11 | Odd.11b | ~Odd.11b | 11 | Evn.11b | ~Evn.11b |     |     |    |    | <- q_11_11_10, q_spr_sum_odd
         | 10 | Odd.10  | ~Odd.10  | 10 | Evn.10  | ~Evn.10  |     |     |    |    |
 
-        with constraints enforcing:
+        with constraints of:
 
         1) applying the plain-spreaded lookup on 11-11-10 limbs of Evn and Odd:
              Odd: (Odd.11a, Odd.11b, Odd.10)
@@ -337,6 +397,10 @@ impl<F: PrimeField> RipeMD160Chip<F> {
         )
     }
 
+    /// Given a u64 value representing a spreaded value, this function
+    /// decomposes it into 11-11-10 limbs for both its even and odd bits,
+    /// depending on `even_or_odd` it assigns them in the circuit, and returns
+    /// the assigned word corresponding to either the even or odd part.
     fn assign_sprdd_11_11_10(
         &self,
         region: &mut Region<'_, F>,
@@ -527,3 +591,117 @@ impl<F: PrimeField> RipeMD160Chip<F> {
 //     |  T0 |   A0  |   A1   |  T1 |   A2  |   A3   |   A4  | A5 | A6 | A7 |
 //     |-----|-------|--------|-----|-------|--------|-------|----|----|----|
 //     |  2  | carry | ~carry |  0  |   0   |   ~0   |   A   |  B |  C |  D | <- q_mod_add
+
+#[cfg(test)]
+mod tests {
+    use core::cmp::max;
+
+    use midnight_curves::Fq as F;
+    use midnight_proofs::{circuit::SimpleFloorPlanner, dev::MockProver, plonk::Circuit};
+
+    use super::*;
+    use crate::{
+        field::{
+            decomposition::{chip::P2RDecompositionConfig, pow2range::Pow2RangeChip},
+            native::{NB_ARITH_COLS, NB_ARITH_FIXED_COLS},
+            AssignedNative,
+        },
+        testing_utils::FromScratch,
+    };
+
+    struct TestCircuit<F: PrimeField> {
+        inputs: [F; 5],
+    }
+
+    impl<F: PrimeField> Circuit<F> for TestCircuit<F> {
+        type Config = (RipeMD160Config, P2RDecompositionConfig);
+        type FloorPlanner = SimpleFloorPlanner;
+        type Params = ();
+
+        fn without_witnesses(&self) -> Self {
+            unreachable!()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+            let advice_columns = (0..max(NB_ARITH_COLS, NB_RIPEMD160_ADVICE_COLS))
+                .map(|_| meta.advice_column())
+                .collect::<Vec<_>>();
+
+            let fixed_columns = (0..max(NB_ARITH_FIXED_COLS, NB_RIPEMD160_FIXED_COLS))
+                .map(|_| meta.fixed_column())
+                .collect::<Vec<_>>();
+
+            let instance_columns = [meta.instance_column(); 2];
+
+            let native_config = NativeChip::configure(
+                meta,
+                &(
+                    advice_columns[..NB_ARITH_COLS].try_into().unwrap(),
+                    fixed_columns[..NB_ARITH_FIXED_COLS].try_into().unwrap(),
+                    instance_columns,
+                ),
+            );
+
+            let pow2range_config = Pow2RangeChip::configure(meta, &advice_columns[1..=4]);
+            let core_decomposition_config =
+                P2RDecompositionChip::configure(meta, &(native_config, pow2range_config));
+
+            let ripemd160_config = RipeMD160Chip::configure(
+                meta,
+                &(
+                    advice_columns[..NB_RIPEMD160_ADVICE_COLS].try_into().unwrap(),
+                    fixed_columns[..NB_RIPEMD160_FIXED_COLS].try_into().unwrap(),
+                ),
+            );
+
+            (ripemd160_config, core_decomposition_config)
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<F>,
+        ) -> Result<(), Error> {
+            let native_gadget = NativeGadget::new_from_scratch(&config.1);
+            let ripemd160_chip = RipeMD160Chip::new(&config.0, &native_gadget);
+
+            native_gadget.load_from_scratch(&mut layouter)?;
+            ripemd160_chip.load(&mut layouter)?;
+
+            let inputs: Vec<AssignedNative<F>> =
+                ripemd160_chip.native_gadget.assign_many_fixed(&mut layouter, &self.inputs)?;
+
+            let assigned_inputs: Vec<AssignedWord<F>> =
+                inputs.into_iter().map(AssignedWord).collect();
+
+            // Iterate over inputs and apply the spread operation
+            for (assigned_input, &input) in assigned_inputs.iter().zip(self.inputs.iter()) {
+                let res = ripemd160_chip.prepare_spreaded(&mut layouter, assigned_input)?;
+                res.0
+                    .value()
+                    .assert_if_known(|res| **res == u64_to_fe(spread(fe_to_u32(input))))
+            }
+
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_prepare_spreaded() {
+        let circuit = TestCircuit {
+            inputs: [
+                F::from(0u64),
+                F::from(1u64),
+                F::from(42u64),
+                F::from(255u64),
+                F::from(1023u64),
+            ],
+        };
+
+        let prover = match MockProver::<F>::run(16, &circuit, vec![vec![]]) {
+            Ok(prover) => prover,
+            Err(e) => panic!("{:?}", e),
+        };
+        assert_eq!(prover.verify(), Ok(()));
+    }
+}
