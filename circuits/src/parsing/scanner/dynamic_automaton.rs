ee974a131aae1f43879f3b5471382578644a12a7
Itsaka Rakotonirina <itsaka.rakotonirina@gmail.com>
2026-02-13 14:54:35 +0100

    Dynamic automaton tests

--- circuits/src/parsing/scanner/dynamic_automaton.rs
+++ circuits/src/parsing/scanner/dynamic_automaton.rs
@@ -322,3 +322,260 @@ where
         )
     }
 }
+#[cfg(test)]
+mod test {
+    use ff::PrimeField;
+    use itertools::Itertools;
+    use midnight_proofs::{
+        circuit::{Layouter, SimpleFloorPlanner, Value},
+        dev::MockProver,
+        plonk::{Circuit, ConstraintSystem, Error},
+    };
+
+    use super::super::{regex::Regex, ScannerChip};
+    use crate::{
+        field::AssignedNative,
+        instructions::{AssertionInstructions, AssignmentInstructions},
+        testing_utils::FromScratch,
+        types::AssignedByte,
+        utils::circuit_modeling::circuit_to_json,
+    };
+
+    /// A circuit that parses two inputs against dynamically-provided regexes
+    /// using `parse_dynamic`. When both regexes are equal, the second call
+    /// should hit the cache. `must_cache` controls whether this is asserted.
+    #[derive(Clone, Debug)]
+    struct DynamicRegexCircuit<F: PrimeField> {
+        regex1: Regex,
+        input1: Vec<Value<u8>>,
+        output1: Vec<Value<F>>,
+        regex2: Regex,
+        input2: Vec<Value<u8>>,
+        output2: Vec<Value<F>>,
+        must_cache: bool,
+    }
+
+    impl<F: PrimeField> DynamicRegexCircuit<F> {
+        fn new(
+            regex1: Regex,
+            input1: &str,
+            output1: &[usize],
+            regex2: Regex,
+            input2: &str,
+            output2: &[usize],
+            must_cache: bool,
+        ) -> Self {
+            Self {
+                regex1,
+                input1: input1.bytes().map(Value::known).collect(),
+                output1: output1.iter().map(|&x| Value::known(F::from(x as u64))).collect(),
+                regex2,
+                input2: input2.bytes().map(Value::known).collect(),
+                output2: output2.iter().map(|&x| Value::known(F::from(x as u64))).collect(),
+                must_cache,
+            }
+        }
+    }
+
+    impl<F> Circuit<F> for DynamicRegexCircuit<F>
+    where
+        F: PrimeField + Ord,
+    {
+        type Config = <ScannerChip<usize, F> as FromScratch<F>>::Config;
+        type FloorPlanner = SimpleFloorPlanner;
+        type Params = ();
+
+        fn without_witnesses(&self) -> Self {
+            unreachable!()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+            let committed_instance_column = meta.instance_column();
+            let instance_column = meta.instance_column();
+            ScannerChip::configure_from_scratch(meta, &[committed_instance_column, instance_column])
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<F>,
+        ) -> Result<(), Error> {
+            let scanner_chip = ScannerChip::<usize, F>::new_from_scratch(&config);
+
+            // First parse.
+            let input1: Vec<AssignedByte<F>> =
+                scanner_chip.native_gadget.assign_many(&mut layouter, &self.input1)?;
+            let output1: Vec<AssignedNative<F>> =
+                scanner_chip.native_gadget.assign_many(&mut layouter, &self.output1)?;
+            let parsed1 = scanner_chip.parse_dynamic(&mut layouter, &self.regex1, &input1)?;
+            assert_eq!(parsed1.len(), output1.len(), "first output length mismatch");
+            parsed1.iter().zip_eq(output1.iter()).try_for_each(|(o1, o2)| {
+                scanner_chip.native_gadget.assert_equal(&mut layouter, o1, o2)
+            })?;
+
+            // Second parse.
+            let input2: Vec<AssignedByte<F>> =
+                scanner_chip.native_gadget.assign_many(&mut layouter, &self.input2)?;
+            let output2: Vec<AssignedNative<F>> =
+                scanner_chip.native_gadget.assign_many(&mut layouter, &self.output2)?;
+            let parsed2 = scanner_chip.parse_dynamic(&mut layouter, &self.regex2, &input2)?;
+            assert_eq!(
+                parsed2.len(),
+                output2.len(),
+                "second output length mismatch"
+            );
+            parsed2.iter().zip_eq(output2.iter()).try_for_each(|(o1, o2)| {
+                scanner_chip.native_gadget.assert_equal(&mut layouter, o1, o2)
+            })?;
+
+            // Check caching: tag counter starts at 1; each cache miss
+            // increments it. With caching, same regex -> 1 miss (counter=2).
+            // Without caching, 2 different regexes -> 2 misses (counter=3).
+            let tag_count = *scanner_chip.dynamic_tag_counter.borrow();
+            if self.must_cache {
+                assert_eq!(
+                    tag_count, 2,
+                    "expected cache hit (1 distinct regex), got {tag_count} tags"
+                );
+            } else {
+                assert_eq!(
+                    tag_count, 3,
+                    "expected cache miss (2 distinct regexes), got {tag_count} tags"
+                );
+            }
+
+            // Load the fixed tables (pow2range etc.). The automaton fixed
+            // table is not loaded since we only use dynamic lookups.
+            scanner_chip.native_gadget.load_from_scratch(&mut layouter)
+        }
+    }
+
+    fn dynamic_basic_test(
+        test_index: usize,
+        cost_model: bool,
+        entry1: (Regex, &str, &[usize]),
+        entry2: (Regex, &str, &[usize]),
+        must_pass: bool,
+        must_cache: bool,
+    ) {
+        assert!(
+            !cost_model || must_pass,
+            ">> [dynamic test {test_index}] (bug) if cost_model is set to true, must_pass should be set to true"
+        );
+        let circuit = DynamicRegexCircuit::<midnight_curves::Fq>::new(
+            entry1.0, entry1.1, entry1.2, entry2.0, entry2.1, entry2.2, must_cache,
+        );
+        let prover = MockProver::<midnight_curves::Fq>::run(11, &circuit, vec![vec![], vec![]]);
+        if must_pass {
+            println!(
+                ">> [dynamic test {test_index}] Parsing inputs '{}' and '{}', which should pass (cache: {must_cache})",
+                entry1.1, entry2.1
+            );
+            prover.unwrap().assert_satisfied()
+        } else {
+            match prover {
+                Ok(prover) => {
+                    if let Ok(()) = prover.verify() {
+                        panic!(
+                            ">> [dynamic test {test_index}] inputs '{}' / '{}' incorrectly accepted",
+                            entry1.1, entry2.1
+                        )
+                    } else {
+                        println!(">> [dynamic test {test_index}] verifier failed (expected)",)
+                    }
+                }
+                Err(_) => println!(">> [dynamic test {test_index}] prover failed (expected)",),
+            }
+        }
+
+        if cost_model {
+            circuit_to_json::<midnight_curves::Fq>(
+                "Scanner",
+                &format!("dynamic parsing perf (input length = {})", entry1.1.len()),
+                circuit,
+            );
+        }
+    }
+
+    #[test]
+    fn dynamic_parsing_test() {
+        let regex1 = Regex::hard_coded_example1();
+        let regex2 = Regex::hard_coded_example0();
+
+        // Two correct inputs with the same regex, cache expected.
+        dynamic_basic_test(
+            0,
+            false,
+            (
+                regex1.clone(),
+                "holy hell !!!",
+                &[0, 1, 2, 1, 0, 0, 1, 2, 2, 0, 1, 1, 1],
+            ),
+            (
+                regex1.clone(),
+                "holyyyy hell !!!",
+                &[0, 1, 2, 1, 1, 1, 1, 0, 0, 1, 2, 2, 0, 1, 1, 1],
+            ),
+            true,
+            true,
+        );
+
+        // Same regex, wrong output markers on second input.
+        dynamic_basic_test(
+            1,
+            false,
+            (
+                regex1.clone(),
+                "holy hell !!!",
+                &[0, 1, 2, 1, 0, 0, 1, 2, 2, 0, 1, 1, 1],
+            ),
+            (regex1.clone(), "holy hell !!!", &[0; 13]),
+            false,
+            true,
+        );
+
+        // Same regex, second input doesn't match (missing space).
+        dynamic_basic_test(
+            2,
+            false,
+            (
+                regex1.clone(),
+                "holy hell !!!",
+                &[0, 1, 2, 1, 0, 0, 1, 2, 2, 0, 1, 1, 1],
+            ),
+            (regex1.clone(), "holy hell!!!", &[0; 12]),
+            false,
+            true,
+        );
+
+        // Two different regexes, no cache expected.
+        dynamic_basic_test(
+            3,
+            false,
+            (
+                regex1.clone(),
+                "holy hell !!!",
+                &[0, 1, 2, 1, 0, 0, 1, 2, 2, 0, 1, 1, 1],
+            ),
+            (regex2, "hello (world)!!!!!", &[0; 18]),
+            true,
+            false,
+        );
+
+        // Performance test for the golden files, using an input of 50 bytes.
+        let perf_input = "holyyyyyyyyy   hell    !!!!!!!!!!!!!!!!!!!!!!!!!!!";
+        #[rustfmt::skip]
+        let perf_output: &[usize] = &[
+            0, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ];
+        dynamic_basic_test(
+            4,
+            true,
+            (regex1.clone(), perf_input, perf_output),
+            (regex1, perf_input, perf_output),
+            true,
+            true,
+        );
+    }
+}
