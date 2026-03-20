f71a5c89b9d81c7c0073370269203fd2458ff080
ambrona <miguel.ambrona@iohk.io>
2026-03-20 16:43:55 +0100

    proofs/cost_model: sync proof-size calculation with the protocol

--- zk_stdlib/src/lib.rs
+++ zk_stdlib/src/lib.rs
@@ -124,6 +124,9 @@ const COMMITMENT_BYTE_SIZE: usize = 48;
 /// Byte size of a serialized BLS12-381 scalar.
 const SCALAR_BYTE_SIZE: usize = 32;
 
+/// Number of instance columns given in committed form in a ZK stdlib proof.
+const NB_COMMITTED_INSTANCES: usize = 1;
+
 /// Architecture of the standard library. Specifies what chips need to be
 /// configured.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Encode, Decode)]
@@ -1773,16 +1776,15 @@ where
         params,
         &pk.pk,
         &circuit,
-        1,
+        NB_COMMITTED_INSTANCES,
         &[com_inst.as_slice(), &pi],
         rng,
     )?;
 
     #[cfg(feature = "check-cost-model")]
     {
-        let model = cost_model(relation, Some(pk.k as u32));
         assert_eq!(
-            model.size,
+            cost_model(relation, Some(pk.k as u32)).size,
             proof.len(),
             "Cost model proof size does not match actual proof size"
         );
@@ -1908,7 +1910,7 @@ pub fn cs_degree(arch: ZkStdLibArch) -> usize {
 /// computed automatically.
 pub fn cost_model<R: Relation>(relation: &R, k: Option<u32>) -> CircuitModel {
     let circuit = MidnightCircuit::from_relation(relation, k);
-    circuit_model::<_, COMMITMENT_BYTE_SIZE, SCALAR_BYTE_SIZE>(&circuit)
+    circuit_model::<_, COMMITMENT_BYTE_SIZE, SCALAR_BYTE_SIZE>(&circuit, NB_COMMITTED_INSTANCES)
 }
 
 /// Finds the optimal `k` (log2 of the circuit size) for the given relation.
