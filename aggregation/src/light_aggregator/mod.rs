24c84eba64a19ee86df5f99fc58c9a798f92a29b
ambrona <miguel.ambrona@iohk.io>
2026-04-08 12:31:29 +0200

    Add copyright notice to files that don't have it
    Co-authored-by: Iñigo Querejeta Azurmendi <31273774+iquerejeta@users.noreply.github.com>
    Co-authored-by: David Nevado <davidnevadoc@users.noreply.github.com>
    Co-authored-by: Zhiyong Gong <63758161+John-Gong-Math@users.noreply.github.com>
    Co-authored-by: Itsaka Rakotonirina <itsaka.rakotonirina@gmail.com>
    Co-authored-by: Reinhard Lüftenegger <34608483+remalue@users.noreply.github.com>

--- aggregation/src/light_aggregator/mod.rs
+++ aggregation/src/light_aggregator/mod.rs
@@ -1,3 +1,16 @@
+// This file is part of MIDNIGHT-ZK.
+// Copyright (C) Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! A light aggregator that can take k finished PLONK proofs and produce a
 //! meta proof that only includes the commitments of the k proofs (all the
 //! scalars are optimized away). This is achieved by in-circuit verifying the
--- curves/src/bls12_381/fp.rs
+++ curves/src/bls12_381/fp.rs
@@ -1,3 +1,16 @@
+// This file is part of MIDNIGHT-ZK.
+// Copyright (C) Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! This module provides an implementation of the BLS12-381 base field `GF(p)`
 //! where `p =
 //! 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab`
