ae69ee105841df0205e545bd60dee534eb0f1c97
ambrona <miguel.ambrona@iohk.io>
2026-06-22 16:50:32 +0200

    proofs: refactor cost model to depend on PCS

--- zk_stdlib/src/lib.rs
+++ zk_stdlib/src/lib.rs
@@ -147,12 +147,6 @@ type Curve25519Chip = ForeignEdwardsEccChip<F, Curve25519, MEP, Curve25519Scalar
 
 const ZKSTD_VERSION: u32 = 2;
 
-/// Byte size of a serialized BLS12-381 G1 commitment (compressed).
-const COMMITMENT_BYTE_SIZE: usize = 48;
-
-/// Byte size of a serialized BLS12-381 scalar.
-const SCALAR_BYTE_SIZE: usize = 32;
-
 /// Number of instance columns given in committed form in a ZK stdlib proof.
 const NB_COMMITTED_INSTANCES: usize = 1;
 
@@ -2146,7 +2140,10 @@ pub fn cs_degree(arch: ZkStdLibArch) -> usize {
 /// computed automatically.
 pub fn cost_model<R: Relation>(relation: &R, k: Option<u32>) -> CircuitModel {
     let circuit = MidnightCircuit::from_relation(relation, k);
-    circuit_model::<_, COMMITMENT_BYTE_SIZE, SCALAR_BYTE_SIZE>(&circuit, NB_COMMITTED_INSTANCES)
+    circuit_model::<_, KZGCommitmentScheme<midnight_curves::Bls12>>(
+        &circuit,
+        NB_COMMITTED_INSTANCES,
+    )
 }
 
 /// Finds the optimal `k` (log2 of the circuit size) for the given relation.
