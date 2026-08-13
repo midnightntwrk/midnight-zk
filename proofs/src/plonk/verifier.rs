f475f4cf76fd0217dbd1c84de5b441d970cf71bb
Itsaka Rakotonirina <itsaka.rakotonirina@gmail.com>
2026-08-13 17:36:20 +0200

    Drop `k` from `PolynomialCommitmentScheme::multi_prepare`
    Following Miguel's advice to remove this parameter. Unused in kzg, and only allows for an early check for degenerate parameters in fflonk that has is caught later anyway.

--- proofs/src/plonk/verifier.rs
+++ proofs/src/plonk/verifier.rs
@@ -360,7 +360,7 @@ where
     // We are now convinced the circuit is satisfied so long as the
     // polynomial commitments open to the correct values, which is true as long
     // as the following accumulator passes the invariant.
-    CS::multi_prepare(&queries, vk.domain.k(), transcript).map_err(|_| Error::Opening)
+    CS::multi_prepare(&queries, transcript).map_err(|_| Error::Opening)
 }
 
 /// Prepares a plonk proof into a PCS instance that can be finalized or
